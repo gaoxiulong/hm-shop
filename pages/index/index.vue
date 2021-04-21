5abd6ad45c0af71814702b2bc6ffa8ca5e1c7fba
gaoxiulong <gaoxiulong139@163.com>
2021-04-21 11:39:14 +0800

    首页推荐商品结构搭建

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,12 @@
 <template>
 	<view class="home">
+		<!-- 轮播图 -->
 		<swiper autoplay circular indicator-dots interval="2000">
 			<swiper-item v-for="item in swiper" :id="item.id">
 				<image :src="item.img"></image>
 			</swiper-item>
 		</swiper>
+		<!-- 导航 -->
 		<view class="nav">
 			<view class="nav_item">
 				<view class="iconfont icon-ziyuan"></view>
@@ -22,7 +24,36 @@
 				<view class="iconfont icon-shipin"></view>
 				<text>视频学习</text>
 			</view>
-
+		</view>
+		<!-- 推荐商品 -->
+		<view class="hot_goods">
+			<view class="tit">推荐商品</view>
+			<view class="goods_list">
+				<view class="goods_item">
+					<image src="../../static/pic/33.jpg"></image>
+					<view class="price">
+						<text>¥2199</text>
+						<text>¥2499</text>
+					</view>
+					<view class="name">主题说明主题说明主题说明主题说明主题说明主题说明主题说明</view>
+				</view>
+				<view class="goods_item">
+					<image src="../../static/pic/33.jpg"></image>
+					<view class="price">
+						<text>¥2199</text>
+						<text>¥2499</text>
+					</view>
+					<view class="name">主题说明</view>
+				</view>
+				<view class="goods_item">
+					<image src="../../static/pic/33.jpg"></image>
+					<view class="price">
+						<text>¥2199</text>
+						<text>¥2499</text>
+					</view>
+					<view class="name">主题说明</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -31,21 +62,30 @@
 	export default {
 		data() {
 			return {
-				swiper: []
+				swiper: [],
+				goods: []
 			}
 		},
 		onLoad() {
 			this.getSwiper()
+			this.getHotGoods()
 		},
 		methods: {
 			async getSwiper() {
-				console.log("我被触发了")
 				const res = await this.$myRuquest({
 					url: '/api/getlunbo'
 				})
 				//获取轮播图
 				this.swiper = res.data.message
-			}
+			},
+			async getHotGoods() {
+				const res = await this.$myRuquest({
+					url: '/api/getgoods?pageindex=1'
+				})
+				//获取商品数据
+				console.log("我被触发了", res)
+				this.goods = res.data.message;
+			},
 
 		}
 	}
@@ -62,32 +102,97 @@
 				width: 100%;
 			}
 		}
-	}
 
-	.nav {
-		display: flex;
 
-		.nav_item {
-			width: 25%;
-			text-align: center;
+		.nav {
+			display: flex;
 
-			view {
-				width: 120rpx;
-				height: 120rpx;
-				background: #b50e03;
-				border-radius: 60rpx;
-				margin: 10px auto;
-				line-height: 120rpx;
-				color: #fff;
-				font-size: 50rpx;
+			.nav_item {
+				width: 25%;
+				text-align: center;
+
+				view {
+					width: 120rpx;
+					height: 120rpx;
+					background: $shop-color;
+					border-radius: 60rpx;
+					margin: 10px auto;
+					line-height: 120rpx;
+					color: #fff;
+					font-size: 50rpx;
+				}
+
+				.icon-tupian {
+					font-size: 45rpx;
+				}
+
+				text {
+					font-size: 30rpx;
+				}
+			}
+		}
+
+		.hot_goods {
+			background: #eee;
+			overflow: hidden;
+			/* 子元素设置阻止冒泡 */
+			margin: 10px 0;
+
+			.tit {
+				height: 50px;
+				line-height: 50px;
+				text-align: center;
+				letter-spacing: 20px;
+				color: $shop-color;
+				background: #FFFFFF;
+				margin: 5px 0;
 			}
 
-			.icon-tupian {
-				font-size: 45rpx;
-			}
+			.goods_list {
+				padding: 0px 15rpx;
+				display: flex;
+				/* 一行显示 */
+				flex-wrap: wrap;
+				/* 换行 */
+				justify-content: space-between;
 
-			text {
-				font-size: 30rpx;
+				/* 两个商品中间有空隙*/
+				.goods_item {
+					background: #FFFFFF;
+					width: 355rpx;
+					margin: 10rpx 0;
+					/* 设置上下边距 */
+					padding: 15rpx;
+					box-sizing: border-box;
+
+					/* 盒子尺寸不变*/
+					image {
+						width: 90%;
+						height: 150px;
+						display: block;
+						/* 让图片居中生效*/
+						margin: 0 auto;
+					}
+
+					.price {
+						color: $shop-color;
+						font-size: 36rpx;
+
+						text:nth-child(2) {
+							color: #C0C0C0;
+							font-size: 28rpx;
+							margin-left: 20rpx;
+							text-decoration: line-through;
+						}
+					}
+
+					.name {
+						font-size: 30rpx;
+						line-height: 50rpx;
+						padding-top: 10rpx;
+						padding-bottom: 15rpx;
+					}
+				}
 			}
 		}
 	}
