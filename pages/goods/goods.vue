5ce9ffe5d5062a73dc0185c952d7943d3042e4fd
gaoxiulong <gaoxiulong139@163.com>
2021-04-21 15:22:38 +0800

    首页导航到黑马超市展示商品（封装goods-list组件）

--- pages/goods/goods.vue
+++ pages/goods/goods.vue
@@ -1,22 +1,44 @@
 <template>
-	<view>
-		商品
+	<view class="goods_list">
+		<goods-list :goods='goods'></goods-list>
 	</view>
 </template>
 
 <script>
+	import goodsList from '../../components/goods-list/goods-list.vue'
 	export default {
 		data() {
 			return {
-				
+				goods: [],
+				pageindex: 1
 			}
 		},
 		methods: {
+			//获取商品数据
+			async getHotGoods() {
+				const res = await this.$myRuquest({
+					url: '/api/getgoods?pageindex='+this.pageindex
+				})
+				//console.log("我被触发了", res)
+				this.goods = res.data.message;
+			}
 			
+		},
+		onLoad() {
+			this.getHotGoods()
+		},
+		components:{
+			"goods-list": goodsList
+		},
+		onReachBottom() {
+			console.log("我触底了")
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.goods_list{
+		background: #C0C0C0;
+	}
 
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -16,21 +16,13 @@
 		<!-- 推荐商品 -->
 		<view class="hot_goods">
 			<view class="tit">推荐商品</view>
-			<view class="goods_list">
-				<view class="goods_item" v-for="item in goods" :id="item.id">
-					<image src="../../static/pic/66.jpg"></image>
-					<view class="price">
-						<text>¥{{item.sell_price}}</text>
-						<text>¥{{item.market_price}}</text>
-					</view>
-					<view class="name">{{item.title}}</view>
-				</view>
-			</view>
+			<goods-list :goods= 'goods'></goods-list>
 		</view>
 	</view>
 </template>
 
 <script>
+	import goodsList from '../../components/goods-list/goods-list.vue'
 	export default {
 		data() {
 			return {
@@ -88,6 +80,9 @@
 				})
 			}
 
+		},
+		components:{
+			"goods-list": goodsList
 		}
 	}
 </script>
@@ -147,54 +142,6 @@
 				color: $shop-color;
 				background: #FFFFFF;
 				margin: 5px 0;
-			}
-
-			.goods_list {
-				padding: 0px 15rpx;
-				display: flex;
-				/* 一行显示 */
-				flex-wrap: wrap;
-				/* 换行 */
-				justify-content: space-between;
-
-				/* 两个商品中间有空隙*/
-				.goods_item {
-					background: #FFFFFF;
-					width: 355rpx;
-					margin: 10rpx 0;
-					/* 设置上下边距 */
-					padding: 15rpx;
-					box-sizing: border-box;
-
-					/* 盒子尺寸不变*/
-					image {
-						width: 90%;
-						height: 150px;
-						display: block;
-						/* 让图片居中生效*/
-						margin: 0 auto;
-						
-					}
-
-					.price {
-						color: $shop-color;
-						font-size: 36rpx;
-						margin: 10rpx 0;
-						text:nth-child(2) {
-							color: #C0C0C0;
-							font-size: 28rpx;
-							margin-left: 20rpx;
-							text-decoration: line-through;
-						}
-					}
-
-					.name {
-						font-size: 30rpx;
-						line-height: 50rpx;
-						padding-top: 10rpx;
-						padding-bottom: 15rpx;
-					}
-				}
 			}
 		}
 	}
