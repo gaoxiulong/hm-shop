41b0f2135ab480ec64de2c75014096d7551a3b66
gaoxiulong <gaoxiulong139@163.com>
2021-04-21 16:11:25 +0800

    首页导航商品列表的上拉加载和下拉刷新

--- pages/goods/goods.vue
+++ pages/goods/goods.vue
@@ -1,6 +1,7 @@
 <template>
 	<view class="goods_list">
 		<goods-list :goods='goods'></goods-list>
+		<view class="isOver" v-if="flag">---------END---------</view>
 	</view>
 </template>
 
@@ -10,35 +11,65 @@
 		data() {
 			return {
 				goods: [],
-				pageindex: 1
+				pageindex: 1,
+				flag: false
 			}
 		},
 		methods: {
 			//获取商品数据
-			async getHotGoods() {
+			async getHotGoods(callback) {
 				const res = await this.$myRuquest({
-					url: '/api/getgoods?pageindex='+this.pageindex
+					url: '/api/getgoods?pageindex=' + this.pageindex
 				})
 				//console.log("我被触发了", res)
-				this.goods = res.data.message;
+				this.goods = [...this.goods, ...res.data.message];
+				callback && callback()  //下拉刷新回调
 			}
-			
+
 		},
 		onLoad() {
-			this.getHotGoods()
+			this.getHotGoods()  //无回调的调用
 		},
-		components:{
+		components: {
 			"goods-list": goodsList
 		},
+		//上拉加载更多功能，默认开启
 		onReachBottom() {
-			console.log("我触底了")
+			console.log("我触底了",this.goods.length)
+			//判断是否还有数据
+			if (this.goods.length < this.pageindex * 10) {
+				return this.flag = true
+			} else {
+				this.pageindex++;
+				this.getHotGoods();
+			}
+		},
+		//下拉刷新功能，pages页手动开启该功能
+		onPullDownRefresh() {
+			console.log("下拉刷新了")
+			this.pageindex = 1
+			this.goods = []
+			this.flag = false
+			setTimeout(()=>{
+				this.getHotGoods(()=>{
+					uni.stopPullDownRefresh()  //停止下拉刷新
+				})
+			},1000)
 		}
 	}
 </script>
 
 <style lang="scss">
-	.goods_list{
+	.goods_list {
 		background: #C0C0C0;
-	}
 
+		.isOver {
+			color: #333333;
+			text-align: center;
+			width: 100%;
+			height: 50rpx;
+			line-height: 50rpx;
+			font-size: 28rpx;
+		}
+	}
 </style>
